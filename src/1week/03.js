d68bf2610f49358270eb1fd09047e0a15f38844c
siny <dltlsgh5@gmail.com>
2022-12-08 19:11:44 +0900

    [assignments] 시니 제출2

--- src/1week/03.js
+++ src/1week/03.js
@@ -1,7 +1,8 @@
 const { accumulate } = require('./02.js');
 
+const filter_왼쪽_아래_삼각형 = (arr, i) => arr.filter((item, j) => j < i);
 function multiDimensionalAccumulate(multiDimensionalArr) {
-  return accumulate(multiDimensionalArr.map((arr, i) => arr.filter((item, j) => j < i)).flat(), 0);
+  return accumulate(multiDimensionalArr.map(filter_왼쪽_아래_삼각형).flat());
 }
 
 exports.multiDimensionalAccumulate = multiDimensionalAccumulate;
