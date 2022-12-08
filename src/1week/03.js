beb9fec75b38612c5c1a13048f0b1b4d9e9bd4ba
Dongmin Ahn <eamon3481@naver.com>
2022-12-08 12:27:45 +0900

    feat : 1week - 03

--- src/1week/03.js
+++ src/1week/03.js
@@ -1,15 +1,29 @@
 function multiDimensionalAccumulate(multiDimensionalArr) {
   let accumulator = 0;
-
-  for (let i = 0; i < multiDimensionalArr.length; i++) {
-    for (let j = 0; j < multiDimensionalArr[i].length; j++) {
-      if (j < i) {
-        accumulator += multiDimensionalArr[i][j];
-      }
-    }
+  const multiDimensionalArrCopy = multiDimensionalArr.slice();
+  for (let i = 0; i < multiDimensionalArrCopy.length; i++) {
+    accumulator += singleDimensionalAccumulate(multiDimensionalArrCopy[i], i);
   }
-
   return accumulator;
 }
 
+function singleDimensionalAccumulate(Arr, index) {
+  let accumulator = 0;
+  const arrCopy = Arr.slice();
+
+  for (let i = 0; i < Arr.length; i++) {
+    if (i < index) {
+      accumulator += arrCopy[i];
+    }
+  }
+  return accumulator;
+}
+
+/**
+ * 이 함수는 매개변수로 전달된 multiDimensionalArr를 복사 없이 그대로 참조하고 있습니다.
+ * 문제점은 실제로 multiDimensionalArr의 값을 변경하면 부수효과가 나타납니다. (암묵적 입력)
+ * 이를 해결하기 위해 multiDimensionalArr을 복사하여 사용하도록 수정할 수도 있습니다.
+ * 또한 이중 for문을 사용하고 있습니다. 계산을 더 작게 쪼갤 수 있으므로 'singleDimensionalAccumulate' 함수를 만들어서
+ * 역할을 분리했습니다.
+ */
 exports.multiDimensionalAccumulate = multiDimensionalAccumulate;
