f257fe8bab8ec093086bed3fe2a90d58a65cfc8e
Dongmin Ahn <eamon3481@naver.com>
2022-12-08 13:02:05 +0900

    feat : deepCopy function

--- src/1week/05.js
+++ src/1week/05.js
@@ -1,5 +1,36 @@
 function deepCopy(obj) {
-  return obj;
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+  if (obj instanceof Date) {
+    return new Date(obj.getTime());
+  }
+
+  if (obj instanceof Array) {
+    return obj.reduce((arr, item, i) => {
+      arr[i] = deepCopy(item);
+      return arr;
+    }, []);
+  }
+
+  if (obj instanceof RegExp) {
+    return new RegExp(obj.source, obj.flags);
+  }
+
+  if (obj instanceof Map) {
+    return new Map(obj);
+  }
+
+  if (obj instanceof Set) {
+    return new Set(obj);
+  }
+
+  if (obj instanceof Object) {
+    return Object.keys(obj).reduce((newObj, key) => {
+      newObj[key] = deepCopy(obj[key]);
+      return newObj;
+    }, {});
+  }
 }
 
 exports.deepCopy = deepCopy;
