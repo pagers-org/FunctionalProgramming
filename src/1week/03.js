38650502bc1504442e41ef73c806e93272463358
Dongmin Ahn <eamon3481@naver.com>
2022-12-08 12:37:39 +0900

    refactor : 1week - 03

--- src/1week/03.js
+++ src/1week/03.js
@@ -1,16 +1,17 @@
 function multiDimensionalAccumulate(multiDimensionalArr) {
+  const getCondition = index => innerIndex => innerIndex < index;
   return multiDimensionalArr.reduce(
-    (accumulator, currentArr, index) => accumulator + singleDimensionalAccumulate(currentArr, index),
+    (accumulator, currentArr, index) => accumulator + singleAccumulateByCondition(currentArr, getCondition(index)),
     0,
   );
 }
 
-function singleDimensionalAccumulate(Arr, index) {
+function singleAccumulateByCondition(Arr, condition) {
   let accumulator = 0;
   const arrCopy = Arr.slice();
 
   for (let i = 0; i < Arr.length; i++) {
-    if (i < index) {
+    if (condition(i)) {
       accumulator += arrCopy[i];
     }
   }
@@ -23,5 +24,6 @@ function singleDimensionalAccumulate(Arr, index) {
  * 이를 해결하기 위해 multiDimensionalArr을 복사하여 사용하도록 수정할 수도 있습니다.
  * 또한 이중 for문을 사용하고 있습니다. 계산을 더 작게 쪼갤 수 있으므로 'singleDimensionalAccumulate' 함수를 만들어서
  * 역할을 분리했습니다.
+ * i < index 는 이 함수의 비지니스 로직입니다. 이를 분리하면 더 좋은 코드가 될 수 있습니다.
  */
 exports.multiDimensionalAccumulate = multiDimensionalAccumulate;
