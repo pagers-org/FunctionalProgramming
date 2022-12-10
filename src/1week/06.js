443b7866e58977f832880d59585230ad6ad7ec89
Hun <hun@nextunicorn.kr>
2022-12-10 17:51:53 +0900

    Feat: 2주차 과제 1차 리팩토링 (전역 변수 일부 제거)

--- src/1week/06.js
+++ src/1week/06.js
@@ -1,19 +1,22 @@
-var shopping_cart = [];
-var shopping_cart_total = 0;
+const shopping_cart = [];
+let shopping_cart_total = 0;
 
-function add_item_to_cart(name, price) {
-  shopping_cart.push({
-    name: name,
-    price: price
-  });
+export function add_item_to_cart(cart, name, price) {
+  const newCart = [
+    ...cart,
+    {
+      name: name,
+      price: price,
+    },
+  ];
   calc_cart_total();
 }
 
-function update_shipping_icons() {
-  var buy_buttons = get_buy_button_dom();
-  for (var i = 0; i < buy_buttons.length; i++) {
-    var button = buy_buttons[i];
-    var item = button.item;
+export function update_shipping_icons() {
+  const buy_buttons = get_buy_button_dom();
+  for (let i = 0; i < buy_buttons.length; i++) {
+    const button = buy_buttons[i];
+    const item = button.item;
     if (item.price + shopping_cart_total >= 20) {
       button.show_free_shipping_icon();
     } else {
@@ -22,21 +25,22 @@ function update_shipping_icons() {
   }
 }
 
-function update_tax_dom() {
-  set_tax_dom(shopping_cart_total * 0.10);
+export function update_tax_dom() {
+  set_tax_dom(shopping_cart_total * 0.1);
 }
 
-function calc_cart_total() {
-  shopping_cart_total = 0;
-  for(var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
-    shopping_cart_total += item.price;
-  }
+export function calc_cart_total(carts) {
+  const shopping_cart_total = carts.reduce((acc, cur) => {
+    return acc + cur.price;
+  }, 0);
+
   set_cart_total_dom();
   update_shipping_icons();
   update_tax_dom();
+
+  return shopping_cart_total;
 }
 
-function set_tax_dom() {}
-function set_cart_total_dom() {}
-function get_buy_button_dom() {}
+export function set_tax_dom() {}
+export function set_cart_total_dom() {}
+export function get_buy_button_dom() {}
