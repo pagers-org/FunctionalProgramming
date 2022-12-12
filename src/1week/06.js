995242a0715e54699d0982c041d20316d3717ba4
hyunjinkim <71summernight@naver.com>
2022-12-12 14:08:27 +0900

    1주차 과제

--- src/1week/06.js
+++ src/1week/06.js
@@ -1,46 +1,32 @@
-const shopping_cart = [];
-let shopping_cart_total = 0;
+let shopping_cart = [];
+const MINPRICEOFFREESHIPPING  = 20
 
-export function add_item_to_cart(cart, name, price) {
-  const newCart = [
-    ...cart,
-    {
-      name: name,
-      price: price,
-    },
-  ];
-  calc_cart_total();
+// C 
+const tax = (totalPrice) => totalPrice * 0.1;
+const cart_total_price = (carts)=> carts.reduce((acc, cur) => acc + cur.price, 0);
+const add_item = (carts, item) => [...carts, item];
+const is_free_shipping = (totalPrice) => totalPrice >= minPriceOfFreeShipping;
+
+// A 전역변수 사용, 돔 변경
+export const add_item_to_cart = (item) => {
+  shopping_cart = add_item(shopping_cart, item)
+  update_total_dom();
 }
-
-export function update_shipping_icons() {
+// A 돔읽음, 변경
+export const update_shipping_icons = (totalPrice) => {
   const buy_buttons = get_buy_button_dom();
-  for (let i = 0; i < buy_buttons.length; i++) {
-    const button = buy_buttons[i];
-    const item = button.item;
-    if (item.price + shopping_cart_total >= 20) {
-      button.show_free_shipping_icon();
-    } else {
-      button.hide_free_shipping_icon();
-    }
-  }
+  buy_buttons.forEach((button) => {
+    is_free_shipping(totalPrice) ?
+      button.show_free_shipping_icon() :  button.hide_free_shipping_icon()
+  })
+}
+// A 돔변경
+export const update_total_dom = () => {
+  set_cart_total_dom(cart_total_price);
+  update_shipping_icons(cart_total_price);
+  set_tax_dom(tax);
 }
 
-export function update_tax_dom() {
-  set_tax_dom(shopping_cart_total * 0.1);
-}
-
-export function calc_cart_total(carts) {
-  const shopping_cart_total = carts.reduce((acc, cur) => {
-    return acc + cur.price;
-  }, 0);
-
-  set_cart_total_dom();
-  update_shipping_icons();
-  update_tax_dom();
-
-  return shopping_cart_total;
-}
-
-export function set_tax_dom() {}
-export function set_cart_total_dom() {}
-export function get_buy_button_dom() {}
+export const set_tax_dom = () => {}
+export const set_cart_total_dom = () => {}
+export const get_buy_button_dom = () => {}
