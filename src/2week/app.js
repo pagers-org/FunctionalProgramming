05978d08e4bed420ef02c58288cece35ab152935
11t518s <11t518s20161759@gmail.com>
2022-12-16 17:49:39 +0900

    스터디 시간에 했던 부분까지 저장용 커밋

--- src/2week/app.js
+++ src/2week/app.js
@@ -1,68 +1,65 @@
-var shopping_cart = [];
-var shopping_cart_total = 0;
-
-document.querySelectorAll('button').forEach(button =>
-  button.addEventListener('click', ({ target }) => {
-    const name = target.parentNode.querySelector('.menu-name').textContent;
-    const category = target.parentNode.querySelector('.category').textContent;
-    const price = target.parentNode.querySelector('.price').textContent;
-
-    add_item_to_cart({ name, category, price });
-  }),
-);
-
-function add_item_to_cart(item) {
-  shopping_cart.push(item);
-  console.log(shopping_cart);
-  calc_cart_total();
+// 데이터
+let shoppingCart = []
+const TAX_RATE = 0.1
+const setShoppingCart = newShoppingCart => {
+  shoppingCart = newShoppingCart
 }
 
-function calc_cart_total() {
-  shopping_cart_total = 0;
-  for (var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
-    shopping_cart_total += item.price;
-  }
-  set_cart_total_dom();
-  update_shipping_icons();
-  update_tax_dom();
+// 클릭 액션
+document.querySelectorAll("button").forEach(button =>
+  button.addEventListener("click", ({ target }) => {
+    const name = target.parentNode.querySelector(".menu-name").textContent
+    const category = target.parentNode.querySelector(".category").textContent
+    const price = target.parentNode.querySelector(".price").textContent
+
+    const newCart = addItemToCart({ name, category, price })
+    setShoppingCart(newCart)
+    const cartTotalPrice = calcCartTotal(newCart)
+
+    handleDomUpdate(newCart, cartTotalPrice)
+  })
+)
+
+const addItemOnArray = (array, item) => [...array, item]
+const getTotalFromArray = (array, element) => array.reduce((acc, cur) => acc + cur[`${element}`], 0)
+const updateTax = price => price * TAX_RATE
+const updateTaxDom = shoppingCartTotal => setTaxDom(updateTax(shoppingCartTotal))
+const addItemToCart = cartItem => addItemOnArray(shoppingCart, cartItem)
+const calcCartTotal = shoppingCart => getTotalFromArray(shoppingCart, "price")
+
+const addShowOrHideShoppingIconOnArray = array =>
+  array.map(item => ({
+    ...item,
+    showFreeShoppingIcon: () => {
+      console.log("DOM 의 아이콘을 보여줍니다")
+    },
+    hideFreeShoppingIcon: () => {
+      console.log("DOM 의 아이콘을 숨깁니다")
+    },
+  }))
+
+const handleDomUpdate = (shoppingCart, shoppingCartPriceTotal) => {
+  setCartTotalDom(shoppingCartPriceTotal)
+  const buyButtons = addShowOrHideShoppingIconOnArray(shoppingCart)
+
+  updateShippingIcons(buyButtons, shoppingCartPriceTotal)
+  updateTaxDom(shoppingCartPriceTotal)
 }
 
-function set_cart_total_dom() {
-  document.querySelector('.total-price').textContent = shopping_cart_total;
+const updateShippingIcons = (buyButtons, shoppingCartPriceTotal) => {
+  buyButtons.forEach(buyButton => {
+    if (buyButton.price + shoppingCartPriceTotal >= 20) {
+      buyButton.showFreeShoppingIcon()
+    } else {
+      buyButton.hideFreeShoppingIcon()
+    }
+  })
 }
 
-function update_shipping_icons() {
-  var buy_buttons = get_buy_buttons_dom();
-  for (var i = 0; i < buy_buttons.length; i++) {
-    var item = buy_buttons[i];
-    console.log(item);
-    if (item.price + shopping_cart_total >= 20) item.show_free_shopping_icon();
-    else item.hide_free_shopping_icon();
-  }
+// dom을 변경하는 로직
+const setTaxDom = tax => {
+  document.querySelector(".total-price").textContent = tax
 }
-
-function get_buy_buttons_dom() {
-  var buttons = [];
-
-  for (var i = 0; i < shopping_cart.length; i++) {
-    var item = shopping_cart[i];
-    item.show_free_shopping_icon = function () {
-      console.log('DOM 의 아이콘을 보여줍니다');
-    };
-    item.hide_free_shopping_icon = function () {
-      console.log('DOM 의 아이콘을 숨깁니다');
-    };
-    buttons.push(item);
-  }
-
-  return buttons;
-}
-
-function update_tax_dom() {
-  set_tax_dom(shopping_cart_total * 0.1);
-}
-
-function set_tax_dom(value) {
-  document.querySelector('.total-price').textContent = value;
+const setCartTotalDom = shoppingCartTotal => {
+  document.querySelector(".total-price").textContent = shoppingCartTotal
 }
