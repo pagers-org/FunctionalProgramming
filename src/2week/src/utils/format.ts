94c33e524ac612375e6dffe0ecd0263997de3242
areumsheep <areumsheep@gmail.com>
2022-12-21 02:43:18 +0900

    Add: format 함수 추가
    - 특수문자, 한글 제거하는 함수
    - string을 number로 변경하는 함수
    - 3자리마다 숫자에 콤마 추가하는 함수

--- src/2week/src/utils/format.ts
+++ src/2week/src/utils/format.ts
@@ -1,1 +1,14 @@
-export const 문자를_숫자로_변환 = (s: string) => parseInt(s.replace(',', '')); 
+const removeCharacters = (s: string) => {
+  const regexp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"가-힣]/g;
+  return s.replace(regexp, '');
+};
+
+export const toNumber = (s: string) => {
+  const str = removeCharacters(s);
+  return parseInt(str);
+}; 
+
+export const toCommaNumber = (num: number) => {
+  const regexp = /\B(?=(\d{3})+(?!\d))/g;
+  return num.toString().replace(regexp, ',');
+}
