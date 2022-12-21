82ee4e53250cfa82c643144e2177a2614f3202d7
areumsheep <areumsheep@gmail.com>
2022-12-21 15:43:22 +0900

    Add: UI 테스트 추가
    - 크롬에서 확인할 수 있도록 cypress open 명령어 추가

--- src/2week/cypress/e2e/app.cy.js
+++ src/2week/cypress/e2e/app.cy.js
@@ -11,4 +11,22 @@ describe('FECrash 카페', () => {
       cy.get('.total-price').should('have.text', '0원');
     });
   });
+
+  context('상품 추가하여 장바구니 확인', () => {
+    it('첫번째 상품을 장바구니에 추가할 경우 장바구니엔 1,100원이 표시되야 한다.', () => {
+      cy.get('.add-to-cart').first().click();
+      cy.get('.total-price').should('have.text', '1,100원');
+    });
+    it('모든 상품을 하나씩 추가할 경우 장바구니엔 61,270원이 표시되야 한다.', () => {
+      cy.get('.add-to-cart').click({ multiple: true });
+      cy.get('.total-price').should('have.text', '61,270원');
+    });
+  });
+
+  context('상품 추가하여 무료배송 확인', () => {
+    it('모든 상품을 하나씩 추가할 경우 모든 상품에 무료 배송 아이콘이 있어야 한다.', () => {
+      cy.get('.add-to-cart').click({ multiple: true });
+      cy.get('.free-delivery').should('have.css', 'display', 'block');
+    });
+  });
 });
