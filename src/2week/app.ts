3ba9183ecfc2a96351f0e438ddfc2429b7e3a40d
ryuna <anottrx@gmail.com>
2023-01-08 20:43:36 +0900

    🎨 리팩터링
    Co-authored-by: OD <kg-te@naver.com>

--- src/2week/app.ts
+++ src/2week/app.ts
@@ -6,61 +6,48 @@ interface ShoppingCartType {
 
 let shoppingCart: ShoppingCartType[] = [];
 
-document.querySelectorAll('button').forEach(button =>
+document.querySelectorAll('button').forEach((button) => 
   button.addEventListener('click', ({ target }) => {
-    // console.log(target);
     const name = (target as Node).parentNode?.querySelector('.menu-name')?.textContent || '';
     const category = (target as Node).parentNode?.querySelector('.category')?.textContent || '';
     const price = (target as Node).parentNode?.querySelector('.price')?.textContent || '';
     const priceNumber = getNumberFromString(price);
 
-    addItemToCart({ name, category, price: priceNumber });
-    calcCartTotal();
-  }),
+    const cart = addItemToCart({ name, category, price: priceNumber }, shoppingCart);
+    const cartTotal = calcCartTotal(cart); 
+    const tax = getTax(cartTotal);
+    
+    updateDom('.cart-price', cartTotal);
+    updateDom('.tax', tax);
+
+    const total = getTotalPrice(cartTotal, tax)
+    updateDom('.total-price', total);
+    updateShippingIcons(total);
+
+    shoppingCart = cart;
+  })
 );
 
-const calcCartTotal = () => {
-  const shoppingCartTotal = getTotalCartPrice(shoppingCart);
 
-  setCartTotalDom(shoppingCartTotal);
-  updateShippingIcons(shoppingCartTotal);
-  updateTaxDom(shoppingCartTotal);
-}
 
-const setCartTotalDom = (shoppingCartTotal: number) => (document.querySelector('.total-price') as HTMLElement).textContent = makeKorFormat(shoppingCartTotal);
+const updateShippingIcons = (total: number) => {
+  if (isOver(total, FREE_SHIPPING_PRICE)) (document.querySelector('.free-shipping') as HTMLElement).style.display = 'block';
+};
 
-const updateShippingIcons = (shoppingCartTotal: number) => {
-  const buyButtons = getBuyButtonsDom();
-  buyButtons.forEach((buyButton) => {
-    if (isOver(buyButton.price + shoppingCartTotal, 20000)) buyButton.showFreeShoppingIcon();
-    else buyButton.hideFreeShoppingIcon();
-  })
-}
-
-const getBuyButtonsDom = () => {
-  return shoppingCart.map((cart) => ({
-    ...cart,
-    showFreeShoppingIcon: () => {
-      (document.querySelectorAll('.free-shipping')).forEach((item) => (item as HTMLElement).style.display = 'block')
-      // console.log("DOM 의 아이콘을 보여줍니다");
-    },
-    hideFreeShoppingIcon: () => {
-      // console.log('DOM 의 아이콘을 숨깁니다');
-    },
-  }));
-}
-
-const updateTaxDom = (shoppingCartTotal: number) => setTaxDom(shoppingCartTotal, getTax(shoppingCartTotal));
-
-const setTaxDom = (shoppingCartTotal: number, tax: number) => (document.querySelector('.total-price') as HTMLElement).textContent = makeKorFormat(getTotalPrice(shoppingCartTotal, tax));
+const updateDom = (classname: string, money: number) => (document.querySelector(classname) as HTMLElement).textContent = makeKorFormat(money);
 
 // 계산
 const TAX_RATE = 0.1;
+const FREE_SHIPPING_PRICE = 20000;
 
-const addItemToCart = (item: ShoppingCartType) => shoppingCart = [...shoppingCart, item];
+const calcCartTotal = (cart: ShoppingCartType[]) => cart.reduce((prev, cur) => prev + cur.price, 0);
+
+const addItemToCart = (item: ShoppingCartType, shoppingCart: ShoppingCartType[]) => {
+  const cart = [...shoppingCart, item];
+  return cart;
+}
 
 const getNumberFromString = (input: string) => Number(input.replace(/,|원/g, ''));
-const getTotalCartPrice = (cart: ShoppingCartType[]) => cart.reduce((prev, cur) => prev + cur.price, 0);
 
 const getTax = (price: number) => price * TAX_RATE;
 const getTotalPrice = (price: number, tax: number) => price + tax;
