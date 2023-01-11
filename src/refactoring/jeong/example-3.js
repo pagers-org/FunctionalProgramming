b0174866a338c6074e7b7c6e5b4685770a5cfa34
jlee0505 <hj960322@gmail.com>
2023-01-12 00:51:27 +0900

    refactor: example-3 리팩토링

--- src/refactoring/jeong/example-3.js
+++ src/refactoring/jeong/example-3.js
@@ -7,14 +7,26 @@ async function getUserData({ id }) {
     `https://jsonplaceholder.typicode.com/users?id=${id}`
   );
 }
+async function saveUserData({ id }) {
+  // TODO
+}
 
 var logToSnapErrors = (error) =>
   console.log(`🚫 에러가 발생했어요: ${error.message}`);
 
-//
-
-try {
+const withLogging = (f) => {
+  try {
+    f();
+  } catch (error) {
+    logToSnapErrors(error);
+  }
+};
+withLogging(() => {
   getUserData(user);
-} catch (error) {
-  logToSnapErrors(error);
-}
+});
+withLogging(() => {
+  saveUserData(user);
+});
+/**
+ * 리팩토링: withLogging() 함수로 만들어 본문이 되는 함수를 인자로 받음으로써 여러 함수의 error를 보내야 하는 snap errors 서비스가 용이해졌습니다.
+ */
