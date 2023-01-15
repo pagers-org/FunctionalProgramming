e12dbd124077212882d9ee2016255184899cf51c
areumsheep <areumsheep@gmail.com>
2023-01-15 20:03:24 +0900

    Add: 과일, 야채, 자동차 조건부 수정

--- src/refactoring/areumsheep/수정후-01_조건부_복잡성.js
+++ src/refactoring/areumsheep/수정후-01_조건부_복잡성.js
@@ -43,3 +43,55 @@ console.log(getAnimalDetails());
 console.log(getAnimalDetails({ type: 'dog', gender: 'female' }));
 console.log(getAnimalDetails({ type: 'dog', name: 'Lucy' }));
 console.log(getAnimalDetails({ type: 'dog', name: 'Lucy', gender: 'female' }));
+
+// 과일 색 출력
+const printFruits = (color = []) => {
+  switch (color) {
+    case 'red':
+      return ['apple', 'strawberry'];
+    case 'yellow':
+      return ['banana', 'pineapple'];
+    case 'purple':
+      return ['grape', 'plum'];
+  }
+};
+console.log(printFruits(null));
+console.log(printFruits('yellow'));
+
+// 야채 이름 출력
+const printVegetableName = (vegetable) => {
+  if (!(vegetable && vegetable.name)) {
+    console.log('unknown');
+    return;
+  }
+  console.log(vegetable.name);
+};
+printVegetableName(undefined);
+printVegetableName({});
+printVegetableName({ name: 'cabbage', quantity: 2 });
+
+// 자동차 프로퍼티 확인
+const car = {
+  model: 'Fiesta',
+  manufacturer: {
+    name: 'Ford',
+    address: {
+      street: 'Some Street Name',
+      number: '5555',
+      state: 'USA',
+    },
+  },
+};
+
+const model = car?.model || 'default model';
+const street = car?.manufacturer?.address?.street || 'default street';
+const phoneNumber = car?.manufacturer?.address && car?.manufacturer?.phoneNumber;
+
+console.log(model);
+console.log(street);
+console.log(phoneNumber);
+
+const isManufacturerFromUSA = () => {
+  console.log(car?.manufacturer?.address?.state === 'USA');
+};
+console.log(isManufacturerFromUSA());
