2312925ad3d3f8e2e763455895b68819061def26
areumsheep <areumsheep@gmail.com>
2023-01-16 11:35:20 +0900

    Update: filter 함수 코드 정리

--- src/refactoring/areumsheep/filter.js
+++ src/refactoring/areumsheep/filter.js
@@ -1,9 +1,8 @@
 const filter = (array, callback) => { 
   let i = -1;
   const result = [];
-  const arrayLength = array.length;
 
-  while (++i < arrayLength) {
+  while (++i < array.length) {
     const condition = callback(array[i], i, array);
     if (condition) result.push(array[i]);
   }
