0e22e468c730f55ad58dbd7c6510950d07dbde1e
feel0321 <dishate@naver.com>
2023-01-18 01:01:29 +0900

    refactor: example2 param 순서 변경

--- src/refactoring/feel/example-2.js
+++ src/refactoring/feel/example-2.js
@@ -1,10 +1,11 @@
-// 반복문이 비슷한 형태로 반복된다
+const foods = ['피자', '치킨', '햄버거'];
 
-const myForeach = (callback, array) => {
+// forEach
+const myForeach = (array, callback) => {
   let i = 0;
   while (i < array.length) {
     callback(array[i++]);
   }
 };
-myForeach(food => console.log(`${food} 먹는다`), ['피자', '치킨', '햄버거']);
-myForeach(dish => console.log(`${dish} 닦는다`), ['접시1', '접시2']);
+
+myForeach(foods, food => console.log(`${food} 먹는다`));
