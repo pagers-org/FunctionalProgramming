30010512ba5f13b4ad689d9448f38c112bc2fb7d
feel0321 <dishate@naver.com>
2023-01-18 01:10:42 +0900

    feat: myForEach를 사용해 myMap 추가

--- src/refactoring/feel/example-2.js
+++ src/refactoring/feel/example-2.js
@@ -1,4 +1,5 @@
 const foods = ['피자', '치킨', '햄버거'];
+const fruits = [{ name: '사과' }, { name: '바나나' }, { name: '수박' }];
 
 // forEach
 const myForeach = (array, callback) => {
@@ -9,3 +10,18 @@ const myForeach = (array, callback) => {
 };
 
 myForeach(foods, food => console.log(`${food} 먹는다`));
+
+// map
+const myMap = (array, callback) => {
+  const result = [];
+  myForeach(array, item => {
+    const part = callback(item);
+    result.push(part);
+  });
+
+  return result;
+};
+
+const resultMap = myMap(fruits, fruit => fruit.name);
+// [ '사과', '바나나', '수박' ]
+console.log(resultMap);
