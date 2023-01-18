89da35be5414c2b043fd7024607bddf78e974e76
Dongmin Ahn <eamon3481@naver.com>
2023-01-19 02:24:00 +0900

    refactor : 03 부적절한 평가

--- src/refactoring/eamon/수정후-03_부적절한_평가.js
+++ src/refactoring/eamon/수정후-03_부적절한_평가.js
@@ -12,7 +12,7 @@ const Client = ({ name, type, location }) => {
   getName = () => this.name;
   getType = () => this.name;
   getLocation = () => this.location;
-  getPriceByProduct = product => product.value - per2Int(product.value, this.offers[this.type]);
+  getPriceByProduct = product => product.getValue() - per2Int(product.getValue(), this.offers[this.type]);
 
   return {
     getName,
@@ -52,13 +52,14 @@ const Order = ({ id, value, client, product }) => {
   getValue = () => this.value;
   getClient = () => this.client;
   getProduct = () => this.product;
-  getTaxes = loc => this.getTaxes(this.taxes[loc]);
+  getTaxes = loc => this.taxes[loc];
 
   return {
     getId,
     getValue,
     getClient,
     getProduct,
+    getTaxes,
   };
 };
 
@@ -67,18 +68,42 @@ const Summary = ({ order }) => {
 
   printSummary = () => {
     let client = order.getClient();
-    let product = order.product();
-
+    let product = order.getProduct();
     return `Order: ${order.getId()}
                 Client: ${client.getName()}
                 Product: ${product.getProductName()}
                 TotalAmount: ${client.getPriceByProduct(product) + this.order.getTaxes(client.getLocation())}
                 
                 
-                Arrival in: ${this.order.product.getShipping()} days.`;
+                Arrival in: ${product.getShipping()} days.`;
   };
 
   return {
     printSummary,
   };
 };
+
+const client = Client({
+  name: 'Eamon',
+  type: 'premium',
+  location: 'EU',
+});
+
+const product = Product({
+  value: 100,
+  name: 'Macbook Pro',
+  shipping: 3,
+});
+
+const order = Order({
+  id: 1,
+  value: 100,
+  client,
+  product,
+});
+
+const summary = Summary({
+  order,
+});
+
+console.log(summary.printSummary());
