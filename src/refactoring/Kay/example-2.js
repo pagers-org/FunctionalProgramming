2e8c2f5f47a19dc6d6d7e382180d96f47cb0eea3
Kay <duddnr654@naver.com>
2023-01-21 15:17:14 +0900

    [FIX]: utils 함수 예외 로직 추가

--- src/refactoring/Kay/example-2.js
+++ src/refactoring/Kay/example-2.js
@@ -1,3 +1,5 @@
+import { _forEach } from "./utils";
+
 const foods = ["cake", "pizza", "ice", "rice"];
 const dishes = ["red", "blue", "green"];
 
@@ -7,19 +9,13 @@ const wash = (item) => console.log(`${item} is clean`);
 const dry = (item) => console.log(`${item} is dry`);
 const putAway = (item) => console.log(`${item} is put`);
 
-const forEach = (items, callBack) => {
-  for (let i = 0; i < items.length; i++) {
-    callBack(items[i]);
-  }
-};
-
-forEach(foods, (food) => {
+_forEach(foods, (food) => {
   if (foods.length === 0) throw new Error("food is not enough");
   cook(food);
   eat(food);
 });
 
-forEach(dishes, (dish) => {
+_forEach(dishes, (dish) => {
   if (foods.length === 0) throw new Error("dish is not enough");
   wash(dish);
   dry(dish);
