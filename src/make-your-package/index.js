f2f5fce7c415b24d1ca233a92ce0ef7db663b876
JUCHEOLJIN <toto006521@gmail.com>
2023-01-25 19:54:47 +0900

    feat: 2부를 여는 코드 작업 추가

--- src/make-your-package/index.js
+++ src/make-your-package/index.js
@@ -1,1 +1,20 @@
 // 여기에 자신의 패키지를 작성할 거에요!
+
+ const forEach = (array, callback) => {
+    for(let i = 0; i < array.length; i++){
+      let item = array[i];
+      callback(item, i, [...array])
+    }
+};
+
+ const map = (array, callback) => {
+  const newArray = [];
+
+  forEach(array, (item,i,array) => newArray.push(callback(item, i, array)));
+  return newArray;
+}
+
+module.exports = {
+    map,
+}
+
