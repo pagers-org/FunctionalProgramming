9048f48e5b840a910245f96d6a36df7e2b7afc65
areumsheep <areumsheep@gmail.com>
2023-01-26 18:37:28 +0900

    Add: 2번 문제 풀이 추가

--- src/3week/problems/2.js
+++ src/3week/problems/2.js
@@ -3,3 +3,14 @@
 // 전, 후에 존재하는 공백은 제거하고
 // 모든 문자열을 비교하고 중복된 값을 제거하고
 // 문자열의 길이 순서대로 반환해주세요.
+
+const { pipe } = require('../utils/pipe');
+const { reduce } = require('../utils/reduce');
+
+const splitByComma = (str) => str.split(',');
+const trimArray = (array) => reduce(array, (acc, value) => acc.concat(value.trim()), []);
+const distinctArray = (array) => [...new Set(array)];
+const compareLength = (array) => array.sort((a, b) => a.length - b.length);
+
+const call = pipe(splitByComma, trimArray, distinctArray, compareLength);
+console.log(call('asfasdfkl , sdf,asdf as  , ddsf,asdfnkl, as, as'));
