ecd9938199599dbd6c5b8eeb455da22d606a0010
areumsheep <areumsheep@gmail.com>
2023-01-30 15:46:02 +0900

    Update: Map으로 unique 체크하도록 변경
    - Set으로 할 경우 object 값을 비교할 경우 체크가 어려움

--- src/4week/utils/unique.test.js
+++ src/4week/utils/unique.test.js
@@ -3,23 +3,28 @@ const {I} = require('../internal_utils');
 
 // 어떤 것을 해볼까요?
 const unique = (arr, callback, isSorted) => {
-  if (I.isIterable(arr)) {
-    if (typeof callback === 'function') {
-      const set = new Set();
-      return arr.reduce((acc, value) => {
-        const test = callback(value);
-        if (!set.has(test)) {
-          acc.push(value);
-        }
-        set.add(test);
-
-        return acc;
-      }, []);
-    }
-    else {
-      return [...new Set(arr)];
-    }
+  if (!I.isIterable(arr)) {
+    return [];
   }
+  const map = new Map();
+  return arr.reduce((acc, item) => {
+    let key;
+    // Object인 경우
+    if (!Array.isArray(item)) {
+      key = JSON.stringify(item);
+    }
+    // callback이 있는 경우
+    if (typeof callback === 'function') {
+      key = callback(item);
+    }
+
+    if (!map.has(key)) {
+      acc.push(item);
+    }
+    map.set(key, item);
+
+    return acc;
+  }, []);
 };
 
 describe('unique 테스트', () => {
@@ -63,6 +68,12 @@ describe('unique 테스트', () => {
         { x: 1, y: 2 },
         { x: 2, y: 1 },
       ]);
+    });
+
+    it('case: 4, arr is not iterable', () => {
+      const errorObjects = unique({ a: 'a', b: 'b' });
+      
+      expect(errorObjects).toEqual([]);
     });
   });
 });
