7dbc2c61770c84ebe2c900e94a6ad7956f58e951
areumsheep <areumsheep@gmail.com>
2023-01-30 16:38:25 +0900

    Rename: Lazy 함수를 모아놓은 `_index` 파일 추가

--- src/4week/utils/lazy/test/reduce.test.js
+++ src/4week/utils/lazy/test/reduce.test.js
@@ -1,11 +1,11 @@
-const { reduce } = require('../reduce');
-const { NL } = require('../../non-lazy');
+const { L } = require('../_index');
+const { NL } = require('../../non-lazy/_index');
 
 describe('reduce 테스트', () => {
   describe('lazy', () => {
     it('case: 1, Advanced', () => {
       const decimalNumbers = [2.3, 2.1, 2.1, 2.1, 2.1, 2.1, 2.1, 3.2, 2.6, 2.8];
-      const addSum = reduce((acc, value) => acc + value);
+      const addSum = L.reduce((acc, value) => acc + value);
 
       expect(addSum(decimalNumbers)).toEqual(NL.reduce(decimalNumbers, (acc, value) => acc + value));
     });
